510fdfa955f2d05297bb58f95c9b5b56ff95ca0f
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-11-28 17:19:35 -0300

    create component currency converter

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <CurrencyConverter baseCurrency="USD" targetCurrency="BRL"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CurrencyConverter from './components/Converter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CurrencyConverter
   }
 }
 </script>
---
+++ src/components/Converter.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="converter">
+    <h2>{{ baseCurrency }} to {{ targetCurrency }}</h2>
+    <input type="text" v-model="baseAmount" v-bind:placeholder="baseCurrency">
+    <input type="button" value="Converter" @click="convert">
+    <h3>{{ targetAmount }}</h3>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'CurrencyConverter',
+  props: ["baseCurrency", "targetCurrency"],
+  data() {
+    return {
+      baseAmount: "",
+      targetAmount: 0,
+      apiKey: process.env.VUE_APP_API_KEY,
+      apiUrl: 'https://openexchangerates.org/api/'
+    }
+  },
+  methods: {
+    async convert() {
+      try {
+        const response = await axios.get(`${this.apiUrl}latest.json?app_id=${this.apiKey}`);
+        const rates = response.data.rates;
+
+        const baseAmount = parseFloat(this.baseAmount);
+        const targetRate = parseFloat(rates[this.targetCurrency]);
+
+        console.log('Base Amount:', baseAmount);
+        console.log('Target Currency Rate:', targetRate);
+
+        if (!isNaN(baseAmount) && !isNaN(targetRate)) {
+          this.targetAmount = (baseAmount * targetRate).toFixed(2);
+          console.log('Converted Amount:', this.targetAmount);
+        } else {
+          console.error('Enter a valid numeric value for conversion.');
+        }
+      } catch (error) {
+        console.error('Error converting currency:', error);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
+
